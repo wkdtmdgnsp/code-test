00e9897a4b21fde3d5247c187b30dbb119ec2156
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-03-20 23:13:42 +0900

    윷놀이

---
+++ src/doit/Test2490.java
@@ -0,0 +1,33 @@
+package doit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Test2490 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Map<Integer, Character> map = new HashMap<>();
+        map.put(3, 'A');
+        map.put(2, 'B');
+        map.put(1, 'C');
+        map.put(0, 'D');
+        map.put(4, 'E');
+
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            while (st.hasMoreTokens()) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            sb.append(map.get(sum) +"\n");
+        }
+
+        System.out.println(sb);
+    }
+}
